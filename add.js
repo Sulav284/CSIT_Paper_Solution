492c0cc097c32aba0e95402a611e40d49de0dcda
Sulav284 <166685026+Sulav284@users.noreply.github.com>
2025-11-30 13:19:59 +0545

    Create ads.js

---
+++ add.js
@@ -0,0 +1,8 @@
+
+(function() {
+    var s = document.createElement("script");
+    s.async = true;
+    s.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9109446393850228";
+    s.crossOrigin = "anonymous";
+    document.head.appendChild(s);
+})();
