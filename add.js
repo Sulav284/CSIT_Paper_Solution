b2c3b95c4e5b6ad5c8d8464a41a97a59a7e04cbf
Sulav284 <166685026+Sulav284@users.noreply.github.com>
2025-11-30 13:46:40 +0545

    Delete add.js

--- add.js
+++
@@ -1,8 +0,0 @@
-
-(function() {
-    var s = document.createElement("script");
-    s.async = true;
-    s.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9109446393850228";
-    s.crossOrigin = "anonymous";
-    document.head.appendChild(s);
-})();
